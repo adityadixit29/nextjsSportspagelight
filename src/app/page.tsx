868c21841a33add703b11cab513503937a78db3b
adityadixit29 <118058546+adityadixit29@users.noreply.github.com>
2024-04-02 00:42:24 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,12 @@ import Homepage from "./pages/Homepage";
 export default function Home() {
   return (
     <>
-      <div className='bg-[#292B32] min-h-screen'>
+      <div className='bg-[#F7F7F8] min-h-screen'>
         <div className='pt-20'>
           <Homepage title={""} totalevents={""} sport={""} src={""} />
         </div>
         <div className='flex justify-center pb-20'>
-          <Spotlight title={""} totalevents={""} sport={""} src={""}/>
+          <Spotlight title={""} totalevents={""} sport={""} src={""} btn={""}/>
         </div>
       </div>
     </>
